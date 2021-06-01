710e4116e223867315ddd27f37641fcc0d7179bd
Roman Haefeli <reduzent@gmail.com>
2021-06-01 13:59:01 +0200

    address linting issues

--- tpf-udp-proxy.py
+++ tpf-udp-proxy.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+This script is part of tpf-server. It relays UDP packets between clients.
+Connections are established by clients sending token messages. If two clients
+send the same token message, subsequent packets are relayed between those
+clients.
+"""
 
 import socket
 import sys
@@ -6,8 +12,8 @@ import sys
 # init values
 UDP_IP = '0.0.0.0'
 DEFAULT_UDP_PORT = 4460
-linklookup = dict()
-tokenlookup = dict()
+LINK_LOOKUP = dict()
+TOKEN_LOOKUP = dict()
 
 try:
     UDP_PORT = int(sys.argv[1])
@@ -18,58 +24,53 @@ except IndexError:
     UDP_PORT = DEFAULT_UDP_PORT
 
 try:
-    assert(1024 <= UDP_PORT <= 65535)
+    assert 1024 <= UDP_PORT <= 65535
 except AssertionError:
     sys.stderr.write("[ERROR] Specified port '%s' is out of valid range (1024-65535).\n" % UDP_PORT)
     sys.exit(1)
 
 # create a socket
 try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((UDP_IP, UDP_PORT))
-except socket.error(msg):
-    sys.stderr.write("[ERROR] %s\n" % msg[1])
+    SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    SOCK.bind((UDP_IP, UDP_PORT))
+except socket.error as error:
+    sys.stderr.write("[ERROR] %s\n" % error)
     sys.exit(1)
 
 def addtoken(token, addr):
-    if token in tokenlookup:
-        if addr not in tokenlookup[token]:
-            tokenlookup[token].append(addr)
-            linklookup[tokenlookup[token][0]] = tokenlookup[token][1]
-            linklookup[tokenlookup[token][1]] = tokenlookup[token][0]
-            del tokenlookup[token]
+    "Remember token for given address or if token has been used already, create a link"
+    if token in TOKEN_LOOKUP:
+        if addr not in TOKEN_LOOKUP[token]:
+            TOKEN_LOOKUP[token].append(addr)
+            LINK_LOOKUP[TOKEN_LOOKUP[token][0]] = TOKEN_LOOKUP[token][1]
+            LINK_LOOKUP[TOKEN_LOOKUP[token][1]] = TOKEN_LOOKUP[token][0]
+            del TOKEN_LOOKUP[token]
     else:
-        tokenlookup[token] = [addr]
+        TOKEN_LOOKUP[token] = [addr]
 
 def clear():
-    linklookup.clear()
-    tokenlookup.clear()
+    "Clear token  and link cache"
+    LINK_LOOKUP.clear()
+    TOKEN_LOOKUP.clear()
 
-# our main loop
 def main():
+    "Main loop listening for incoming packets"
     srcaddr = None
     try:
         while True:
-            data, srcaddr = sock.recvfrom(65507)
+            data, srcaddr = SOCK.recvfrom(65507)
 
             # check token
             if data[0:7] == b'_TOKEN ':
                 token = data.split()
-                try:
-                    addtoken(token[1], srcaddr)
-                except:
-                    pass
+                addtoken(token[1], srcaddr)
 
             # forward data according to lookup table
             else:
-                try:
-                    destaddr = linklookup[srcaddr]
-                    sock.sendto(data, destaddr)
-                except:
-                    pass
+                destaddr = LINK_LOOKUP[srcaddr]
+                SOCK.sendto(data, destaddr)
     except KeyboardInterrupt:
         sys.exit(0)
 
 if __name__ == '__main__':
     main()
-
